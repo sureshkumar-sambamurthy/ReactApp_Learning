bc303a3a018816bbefb54c512b082e1662cf39dd
Suresh kumar Sambamurthy <sureshkumarsambamurthy@Sureshs-MacBook-Pro.local>
2025-09-13 19:05:59 +0530

    updated woodpecker yaml file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['7b2bf8ad74be.ngrok-free.app']
+  }
 })
