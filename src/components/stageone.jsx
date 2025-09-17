143cf537eae29742064098d59c6f3eb3fe4f011c
Suresh kumar Sambamurthy <sureshkumarsambamurthy@Sureshs-MacBook-Pro.local>
2025-09-17 22:01:37 +0530

    Revert "Auto-fixed bug: Fix the below issue in the stageone.jsx component"
    This reverts commit abad5ac2c08ff4011f305d38ea5e1344aca67144.

--- src/components/stageone.jsx
+++ src/components/stageone.jsx
@@ -1,5 +1,6 @@
 import {Button,Form,Alert} from 'react-bootstrap'
-import { MyContext } from '../context/index'; // fixed the pathconst Stage1=()=>{
+import { MyContext } from '../contexteds';
+const Stage1=()=>{
     const playerInput = useRef();
     const context = useContext(MyContext);
     const [error,setError] = useState([false,'']);
