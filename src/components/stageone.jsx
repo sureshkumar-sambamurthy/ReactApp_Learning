862a19b24bc5bff798daf600b2afdc67b3d0a320
Suresh kumar Sambamurthy <sureshkumarsambamurthy@Sureshs-MacBook-Pro.local>
2025-10-06 22:26:58 +0530

    import issues fixed

--- src/components/stageone.jsx
+++ src/components/stageone.jsx
@@ -1,4 +1,5 @@
-import {Button,Form,Alert} from 'react-bootstrap'
+import React, { useContext, useRef, useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap"; 
 import { MyContext } from '../contexteds';
 const Stage1=()=>{
     const playerInput = useRef();
